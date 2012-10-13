1d7f7180a0292703fa0e275a78c29d325f369b0b
Sebastian Borrazas <sebastian@monthlys.com>
2012-10-13 11:24:04 -0200

    Progress on expenses form

--- app/assets/javascripts/views/expenses/new.js
+++ app/assets/javascripts/views/expenses/new.js
@@ -1,45 +1,12 @@
 TripPlanner.Views.ExpensesHandler = (function () {
 
-  /**
-   *
-   */
-  var ExpensesHandler = function (settings) {
-    this.expenses = settings.expenses || [];
-    this.$container = $('#expenses-form');
-    this.templates = {
-      expense: _.template($('#expense-template').html()),
-      alternative: _.template($('#alternative-template').html())
-    };
-    if (this.expenses.length === 0) {
-      this.loadDefaultExpenses();
+  var DEFAULT_ALTERNATIVE = {
+      "cost": "",
+      "person_gap": "per_person",
+      "time_gap": "per_day",
+      "is_checked": false
     }
-    // this.drawAll();
-  };
-
-  /**
-   *
-   */
-  ExpensesHandler.prototype.drawAll = function () {
-    this.$container.empty();
-    this.expenses.forEach(function (expense) {
-      // Create expense template
-      var expenseContent = $(this.templates.expense(expense))
-        , expenseList = expenseContent.find('ul.alternatives');
-
-      expense.alternatives.forEach(function (alternative) {
-        this.templates.alternative(alternative).appendTo(expenseList);
-      }.bind(this));
-
-      // Appending to container
-      this.$container.append(expenseContent);
-    }.bind(this));
-  };
-
-  /**
-   *
-   */
-  ExpensesHandler.prototype.loadDefaultExpenses = function () {
-    this.expenses = [
+    , DEFAULT_EXPENSES = [
       {
         "name": "Accomodation",
         "category": "accomodation",
@@ -48,9 +15,91 @@ TripPlanner.Views.ExpensesHandler = (function () {
       {
         "name": "Transport",
         "category": "transport",
-        "alternatives": []
+        "alternatives": [],
       }
     ];
+
+  /**
+   *
+   */
+  var ExpensesHandler = function (settings) {
+    this.expenses = settings.expenses || [];
+    this.alternativeIndex = 0;
+    this.$mainContainer = $('#expenses-form');
+    this.templates = {
+      expense: _.template($('#expense-template').html()),
+      alternative: _.template($('#alternative-template').html())
+    };
+    if (this.expenses.length === 0) {
+      this.expenses = DEFAULT_EXPENSES;
+    }
+  };
+
+  /**
+   *
+   */
+  ExpensesHandler.prototype.appendAll = function () {
+    this.$mainContainer.empty();
+    this.expenses.forEach(this.appendExpense.bind(this));
+  };
+
+  /**
+   *
+   */
+  ExpensesHandler.prototype.appendExpense = function (expense) {
+    // Create expense template
+    var expenseContent = $(this.templates.expense(expense))
+      , expenseList = expenseContent.find('ul.alternatives');
+
+    // Binding expense content
+    expenseContent.find('.add-alternative').click(function (event) {
+      event.preventDefault();
+
+      this.appendAlternative(expenseList, DEFAULT_ALTERNATIVE);
+    }.bind(this));
+
+    expense.alternatives.forEach(function (alternative) {
+      this.appendAlternative(expenseList, alternative);
+    }.bind(this));
+
+    // Appending default blank alternative
+    this.appendAlternative(expenseList, DEFAULT_ALTERNATIVE);
+
+    // Appending to container
+    this.$mainContainer.append(expenseContent);
+  };
+
+  /**
+   *
+   */
+  ExpensesHandler.prototype.appendAlternative = function (container, alternative) {
+    var $alternative = $(this.templates.alternative({
+      index: this.alternativeIndex,
+      alternative: alternative
+    }));
+
+    if (alternative.person_gap === 'per_person') {
+      $alternative.find('.person_gap .per_person').button('toggle');
+    }
+    else {
+      $alternative.find('.person_gap .per_group').button('toggle');
+    }
+
+    if (alternative.time_gap === 'per_day') {
+      $alternative.find('.time_gap .per_day').button('toggle');
+    }
+    else {
+      $alternative.find('.time_gap .per_stay').button('toggle');
+    }
+
+    this.alternativeIndex += 1;
+    container.append($alternative);
+  };
+
+  /**
+   *
+   */
+  ExpensesHandler.prototype.bindDOMEvents = function () {
   };
 
   return ExpensesHandler;
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,6 +5,11 @@ class Expense < ActiveRecord::Base
   has_many :comments
   has_and_belongs_to_many :travellers
 
+  PERSON_GAPS = %w(per_person per_group)
+  TIME_GAPS = %w(per_day per_stay)
+
   validates :name, presence: true
+  validates :person_gap, inclusion: { in: PERSON_GAPS }
+  validates :time_gap, inclusion: { in: TIME_GAPS }
 
 end
