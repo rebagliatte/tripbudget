4ba466744249aed3ffabe58338adc28f173ccb39
Lucia Escanellas <raviolicode@gmail.com>
2012-10-12 22:17:49 -0200

    Remove example files

--- example/app/models/lala.rb
+++
@@ -1,3 +0,0 @@
-class Lala < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
--- example/db/migrate/20121012215325_create_lalas.rb
+++
@@ -1,8 +0,0 @@
-class CreateLalas < ActiveRecord::Migration
-  def change
-    create_table :lalas do |t|
-
-      t.timestamps
-    end
-  end
-end
