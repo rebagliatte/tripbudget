1014e3ec6d932a80854e450af23c8ace040975d9
Veronica Rebagliatte <vero@monthlys.com>
2012-10-13 20:43:46 -0200

    Abilities and flash messages

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,23 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, [Trip, Destination, Expense, Alternative, Traveller, Comment]
+    if user
+      can :read, Trip, is_public: true
+      can :manage, Trip do |trip|
+        trip.travellers.includes?(user)
+      end
+      can :read, Traveller do |traveller|
+        (user.trips & traveller.trips).any?
+      end
+      can :edit, Traveller, id: user.id
+      can :edit, Destination do |destination|
+        user.trips.includes?(destination.trip)
+      end
+    end
+
+    # Everybody can access these
+    can :read, Traveller do |traveller|
+      traveller.trips.where(is_public: true).any?
+    end
   end
 end
