f004ac026ec81b5383458c37ea5b6738aff1e5a3
Sebastian Borrazas <sebastian@monthlys.com>
2012-10-14 14:22:06 -0200

    Add future trip show

--- app/models/alternative.rb
+++ app/models/alternative.rb
@@ -8,4 +8,19 @@ class Alternative < ActiveRecord::Base
   validates :person_gap, inclusion: { in: PERSON_GAPS }
   validates :time_gap, inclusion: { in: TIME_GAPS }
 
+  def per_group?
+    person_gap == 'per_group'
+  end
+
+  def per_person?
+    person_gap == 'per_person'
+  end
+
+  def per_day?
+    time_gap == 'per_day'
+  end
+
+  def per_stay?
+    time_gap == 'per_stay'
+  end
 end
