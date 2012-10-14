8147572f008017b2cf01570fd3eba5a82859a037
Sebastian Borrazas <sebastian@monthlys.com>
2012-10-14 16:23:08 -0200

    Implement FB!!!

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Tripbudget::Application.routes.draw do
   resources :invitations, only: 'show'
 
   # Authentication
-  match 'auth/twitter/callback', to: 'sessions#create'
+  match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
 
