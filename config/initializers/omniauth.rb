3da93ccbba5442a3febba637f49b19aa9afc4f62
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-04-07 23:31:51 -0300

    Add omniauth credentials handling

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
+omniauth_config = JSON.load(File.join(Rails.root, 'omniauth.json'), symbolize_names: true)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email'
+  provider :twitter, omniauth_config[:twitter_key], omniauth_config[:twitter_secret]
+  provider :facebook, omniauth_config[:facebook_api_key], omniauth_config[:facebook_secret], scope: 'email'
 end
